e837a27f80fd3a232f08a3687a58e45efd781f25
Brett Michalick <bmmage@gmail.com>
2025-02-10 11:19:17 -0500

    abstract add to cart and page views functions

--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -1,3 +1,44 @@
 @if(\Astrogoat\Postscript\Settings\PostscriptSettings::isEnabled() && ! blank(settings('Astrogoat\\Postscript\\Settings\\PostscriptSettings', 'shop_id')))
     <script async src="https://sdk.postscript.io/sdk.bundle.js?shopId={{settings('Astrogoat\\Postscript\\Settings\\PostscriptSettings', 'shop_id')}}"></script>
+    <script defer>
+        window.addEventListener('item-added-to-cart', (event) => {
+            window.postscript.event('add_to_cart', {
+                "shop_id": {{ settings(Astrogoat\Postscript\Settings\PostscriptSettings::class, 'shop_id') }},
+                "url": '{{ request()->url() }}',
+                "search_params": {"variant": event.detail.id},
+                "page_type": "product",
+                "referrer": "{{ request()->headers->get('referer') }}",
+                "resource": {
+                    "category": event.detail.type,
+                    "name": event.detail.title,
+                    "price_in_cents": event.detail.price,
+                    "resource_id": event.detail.product_id,
+                    "resource_type": "product",
+                    "sku": event.detail.sku,
+                    "variant_id": event.detail.id,
+                }
+            });
+        });
+
+        function postscriptPageView(data) {
+            window.addEventListener("postscriptReady", function(ev) {
+                window.postscript.event('page_view', {
+                    "shop_id": {{ settings(Astrogoat\Postscript\Settings\PostscriptSettings::class, 'shop_id') }},
+                    "url": '{{ request()->url() }}',
+                    "search_params": {"variant": data.variant_id},
+                    "page_type": "product",
+                    "referrer": "{{ request()->headers->get('referer') }}",
+                    "resource": {
+                        "category": data.type,
+                        "name": data.name,
+                        "price_in_cents": data.price,
+                        "resource_id": data.product_id,
+                        "resource_type": "product",
+                        "sku": data.sku,
+                        "variant_id": data.variant_id,
+                    }
+                });
+            })
+        }
+    </script>
 @endif
